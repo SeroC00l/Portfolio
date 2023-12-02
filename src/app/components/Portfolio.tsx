a878d5704c0987f5d4cd2c8c37c9527890a6f8da
Sero Cool <aleurrea4@gmail.com>
2023-12-02 15:37:11 -0500

    styling

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -1,99 +1,75 @@
-import React from 'react'
-import Image from "next/image";
-import { External } from "../ico/External";
-import Esat from "../../img/Esat.png";
-import Master_Finance from "../../img/Master_Finance.png";
-import Portfolio from "../../img/Portfolio.png";
+import React from 'react';
+import Image from 'next/image';
+import { External } from '../ico/External';
+import Esat from '../../img/Esat.png';
+import Master_Finance from '../../img/Master_Finance.png';
+import Portfolio from '../../img/Portfolio.png';
 
 export function PortfolioSection() {
+  const gradient = 'linear-gradient( #383838ea, #00eeffea)';
 
-    const gradient = "linear-gradient( #383838ea, #00eeffea)";
+  return (
+    <>
+      <section
+        id="portfolio"
+        className="flex flex-col justify-center items-center bg-gray-700 lg:gap-16 pb-24 pt-24 md:h-screen h-auto"
+      >
+        <h2 className="md:text-4xl text-4xl md:mx-0 mb-4 font-bold text-white">
+          Latest{' '}
+          <span className="inline-block text-homeColor font-bold">Project</span>
+        </h2>
 
-    return (
-        <>
-            <section
-                id="portfolio"
-                className="flex flex-col justify-center items-center bg-gray-700 lg:gap-16 pb-24 pt-24  md:h-screen h-auto"
-            >
-                <h2 className="md:text-4xl text-4xl md:mx-0 mb-4 font-bold text-white">
-                    Latest{" "}
-                    <span className="inline-block text-homeColor font-bold">Project</span>
-                </h2>
-
-                <div className="md:grid grid-cols-3 flex flex-wrap items-center gap-10 mx-20">
-                    <div className="portfolio-box relative rounded-3xl shadow-4xl flex overflow-hidden transition-all">
-                        <Image
-                            src={Esat}
-                            alt="Esat-project"
-                            className="filter-none w-full transition-transform"
-                        />
-                        <div
-                            className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-full"
-                            style={{ background: gradient }}
-                        >
-                            <h4 className="text-2xl text-white">Web App Development</h4>
-                            <p className="text-base text-white">
-                                Web application for a company that provides services for measure
-                                the employe satisfaction level.
-                            </p>
-                            <a
-                                href="https://github.com/SeroC00l/ESAT"
-                                className="text-gray-800 text-3xl h-12 w-12 bg-white rounded-full inline-flex justify-center items-center mt-3"
-                            >
-                                <External />
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className="portfolio-box relative rounded-3xl shadow-4xl flex overflow-hidden">
-                        <Image
-                            src={Portfolio}
-                            alt="Portfolio-project"
-                            className="filter-none w-full transition-transform"
-                        />
-                        <div
-                            className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-full"
-                            style={{ background: gradient }}
-                        >
-                            <h4 className="text-2xl text-white">Web App Development</h4>
-                            <p className="text-base text-white">
-                                My personal web portfolio responsive develop using Nexs js 13,
-                                Tailwind css and firebase.
-                            </p>
-                            <a
-                                href="https://github.com/SeroC00l/Portfolio"
-                                className="text-gray-800 text-3xl h-12 w-12 bg-white rounded-full inline-flex justify-center items-center mt-3"
-                            >
-                                <External />
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className="portfolio-box relative rounded-3xl shadow-4xl flex overflow-hidden transition-all">
-                        <Image
-                            src={Master_Finance}
-                            alt="Master_Finance-project"
-                            className="filter-none w-full transition-transform"
-                        />
-                        <div
-                            className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-full"
-                            style={{ background: gradient }}
-                        >
-                            <h4 className="text-2xl text-white">Multi Platform App</h4>
-                            <p className="text-base text-white">
-                                Multi platform financial website and app develop using HTML, CSS
-                                and Javascript and React Native, also using PHP and PostgeSQL.
-                            </p>
-                            <a
-                                href=""
-                                className="text-gray-800 text-3xl h-12 w-12 bg-white rounded-full inline-flex justify-center items-center mt-3"
-                            >
-                                <External />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+        <div className="md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-4 md:mx-8 lg:mx-20">
+          {projects.map((project, index) => (
+            <div key={index} className="portfolio-box relative rounded-3xl shadow-4xl flex overflow-hidden transition-all">
+              <Image
+                src={project.image}
+                alt={project.alt}
+                className="filter-none w-full transition-transform"
+                width={1200}
+                height={800}
+              />
+              <div
+                className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-full"
+                style={{ background: gradient }}
+              >
+                <h4 className="text-2xl text-white">{project.title}</h4>
+                <p className="text-base text-white">{project.description}</p>
+                <a
+                  href={project.githubLink}
+                  className="text-gray-800 text-3xl h-12 w-12 bg-white rounded-full inline-flex justify-center items-center mt-3"
+                >
+                  <External />
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
+
+const projects = [
+  {
+    title: 'Web App Development',
+    description: 'Web application for a company that provides services for measuring employee satisfaction level.',
+    image: Esat,
+    alt: 'Esat-project',
+    githubLink: 'https://github.com/SeroC00l/ESAT',
+  },
+  {
+    title: 'Web App Development',
+    description: 'My personal web portfolio responsive developed using Next.js 13, Tailwind CSS, and Firebase.',
+    image: Portfolio,
+    alt: 'Portfolio-project',
+    githubLink: 'https://github.com/SeroC00l/Portfolio',
+  },
+  {
+    title: 'Multi Platform App',
+    description: 'Multi-platform financial website and app developed using HTML, CSS, and JavaScript and React Native. Also using PHP and PostgreSQL.',
+    image: Master_Finance,
+    alt: 'Master_Finance-project',
+    githubLink: '', // Add your link here
+  },
+];
--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -7,11 +7,13 @@ import { Android } from "../ico/Android";
 import styled from 'styled-components';
 
 const StyledServicesSection = styled.section`
-  background-color: rgb(31 41 55) ;
-  height: 100vh;
+  background-color: rgb(31 41 55);
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  padding: 2rem;
+  padding-bottom: 4rem;
 
   @media (min-width: 768px) {
     padding-top: 8rem;
@@ -24,6 +26,7 @@ const StyledServiceCard = styled.div`
   border-radius: 1.5rem;
   padding: 1.5rem;
   text-align: center;
+  margin: 1rem;
   flex: 1;
   transition: border-color 0.3s, transform 0.3s;
 
@@ -34,61 +37,35 @@ const StyledServiceCard = styled.div`
 `;
 
 export function Services() {
-    return (
-        <StyledServicesSection id="services">
-            <h2 className="text-4xl text-center mb-8 font-bold text-white">
-                Our{" "}
-                <span className="inline-block text-homeColor font-bold">
-                    Services
-                </span>
-            </h2>
-            <div className="flex text-white flex-wrap gap-8 mx-8">
-                <StyledServiceCard>
-                    <Code />
-                    <h3 className="md:mx-0 text-2xl mb-2">Web Development</h3>
-                    <p className="md:mx-0 text-left md:text-sm text-xs">
-                        I offer web development services that focus on creating innovative
-                        and efficient web applications tailored to meet your business
-                        needs. With my expertise in React, HTML, CSS, JavaScript, and
-                        other web development technologies, I can build intuitive and
-                        user-friendly applications that drive business success. Whether
-                        you need a new website, an e-commerce platform, or a custom web
-                        application, I can provide high-quality and scalable solutions
-                        that meet your requirements. Let's work together to bring your
-                        ideas to life and create a unique online presence for your
-                        business.
-                    </p>
-                </StyledServiceCard>
-                <StyledServiceCard>
-                    <Api />
-                    <h3 className="md:mx-0 text-2xl mb-2">Backend Development</h3>
-                    <p className="md:mx-0 text-left md:text-sm text-xs">
-                        I offer backend development services using various technologies,
-                        including Node.js and Python. I have experience designing and
-                        implementing scalable and secure backend solutions that support a
-                        range of web applications, from e-commerce sites to social media
-                        platforms. My expertise extends to database management, with
-                        proficiency in SQL and MongoDB, and I am committed to delivering
-                        robust and efficient backend systems that meet my clients'
-                        business needs.
-                    </p>
-                </StyledServiceCard>
-                <StyledServiceCard>
-                    <Android />
-                    <h3 className="md:mx-0 text-2xl mb-2">Mobile App Development</h3>
-                    <p className="md:mx-0 text-left md:text-sm text-xs">
-                        I can create innovative and efficient mobile applications using
-                        React Native and Flutter, two of the most popular mobile app
-                        development frameworks in the market. I have experience developing
-                        a range of mobile applications, including social media apps,
-                        e-commerce platforms, and healthcare apps. My focus is on creating
-                        intuitive and user-friendly interfaces that provide a seamless
-                        user experience. I can work with you to develop a custom mobile
-                        application that meets your specific needs and exceeds your
-                        expectations.
-                    </p>
-                </StyledServiceCard>
-            </div>
-        </StyledServicesSection>
-    )
+  return (
+    <StyledServicesSection id="services">
+      <h2 className="text-4xl text-center mb-8 font-bold text-white">
+        Our{" "}
+        <span className="inline-block text-homeColor font-bold">Services</span>
+      </h2>
+      <div className="flex flex-wrap justify-center mx-2">
+        <StyledServiceCard>
+          <Code />
+          <h3 className="text-2xl mb-2 text-white">Web Development</h3>
+          <p className="text-left text-sm text-white m-5">
+            I offer web development services that focus on creating innovative and efficient web applications tailored to meet your business needs. With my expertise in React, HTML, CSS, JavaScript, and other web development technologies, I can build intuitive and user-friendly applications that drive business success. Whether you need a new website, an e-commerce platform, or a custom web application, I can provide high-quality and scalable solutions that meet your requirements. Let's work together to bring your ideas to life and create a unique online presence for your business.
+          </p>
+        </StyledServiceCard>
+        <StyledServiceCard>
+          <Api />
+          <h3 className="text-2xl mb-2 text-white">Backend Development</h3>
+          <p className="text-left text-sm text-white m-5">
+            I offer backend development services using various technologies, including Node.js and Python. I have experience designing and implementing scalable and secure backend solutions that support a range of web applications, from e-commerce sites to social media platforms. My expertise extends to database management, with proficiency in SQL and MongoDB, and I am committed to delivering robust and efficient backend systems that meet my clients' business needs.
+          </p>
+        </StyledServiceCard>
+        <StyledServiceCard>
+          <Android />
+          <h3 className="text-2xl mb-2 text-white">Mobile App Development</h3>
+          <p className="text-left text-sm text-white m-5">
+            I can create innovative and efficient mobile applications using React Native and Flutter, two of the most popular mobile app development frameworks in the market. I have experience developing a range of mobile applications, including social media apps, e-commerce platforms, and healthcare apps. My focus is on creating intuitive and user-friendly interfaces that provide a seamless user experience. I can work with you to develop a custom mobile application that meets your specific needs and exceeds your expectations.
+          </p>
+        </StyledServiceCard>
+      </div>
+    </StyledServicesSection>
+  );
 }
