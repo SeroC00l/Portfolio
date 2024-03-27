4597a786f99a97184cbf0230cc05aee2f68225cb
sero <aleurrea4@gmail.com>
2024-03-27 14:29:52 -0500

    d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "@/globals.css";
 import { Nunito } from "next/font/google";
 import { Menu } from "@/components/Menu";
 import { Header } from "@/components/Header";
